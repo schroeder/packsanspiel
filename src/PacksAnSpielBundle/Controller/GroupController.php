d83a01c40e1b98582ac42ec750900e618d8be63f
Sven Jehles <svenjehles@yahoo.de>
2017-05-26 21:31:34 +0200

    added messaging for game and team

--- src/PacksAnSpielBundle/Controller/GroupController.php
+++ src/PacksAnSpielBundle/Controller/GroupController.php
@@ -3,9 +3,15 @@
 namespace PacksAnSpielBundle\Controller;
 
 use PacksAnSpielBundle\Entity\Team;
+use PacksAnSpielBundle\Entity\Message;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * Group controller.
@@ -28,6 +34,44 @@ class GroupController extends Controller
 
         return $this->render('PacksAnSpielBundle::admin/team/index.html.twig', array(
             'groups' => $groups,
+        ));
+    }
+
+    /**
+     * Sends a message to a Group.
+     *
+     * @Route("/send", name="group_message")
+     */
+    public function sendMessageAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $currentTime = new \DateTime('now');
+
+        $message = new Message();
+        $message->setSendTime($currentTime->getTimestamp());
+        $message->setGame(null);
+
+        $form = $this->createFormBuilder()
+            ->add('teamId', IntegerType::class, array('label' => false, 'required' => true))
+            ->add('messageText', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Nachricht senden'))
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            // data is an array with "gameId", "teamId", and "logLevel" keys
+            $data = $form->getData();
+            $team = $em->getRepository('PacksAnSpielBundle:Team')->find($data['teamId']);
+            $message->setTeam($team);
+            $message->setMessageText($data['messageText']);
+
+            $em->persist($message);
+            $em->flush($message);
+        }
+
+        return $this->render('PacksAnSpielBundle::admin/team/message.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 
